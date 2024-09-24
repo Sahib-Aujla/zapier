d9f7eea90157796a092cd7c9a66f17f4a72ddee0
Sahib.Aujla <sahibpreet568@gmail.com>
2024-09-24 16:16:52 -0400

    added prisma client

--- apps/webhook/src/index.ts
+++ apps/webhook/src/index.ts
@@ -1,15 +1,32 @@
 import express from 'express';
 import { PrismaClient } from '@repo/db';
- 
+
 const client = new PrismaClient();
-const app=express();
+const app = express();
 app.use(express.json());
 
-app.post('/hooks/catch/:userId/:zapId',async(req,res)=>{
-    const {userId, zapId} = req.params;
-    const body=req.body;
+app.post('/hooks/catch/:userId/:zapId', async (req, res) => {
+    const { userId, zapId } = req.params;
+    const body = req.body;
 
-    
+    await client.$transaction(async tx => {
+        const run = await tx.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: body
+            }
+        });;
+
+        await tx.zapRunOutbox.create({
+            data: {
+                zapRunId: run.id
+            }
+        })
+    })
+    res.json({
+        message: "Webhook received"
+    })
+
 })
 
-app.listen(3000);
+app.listen(3002);
