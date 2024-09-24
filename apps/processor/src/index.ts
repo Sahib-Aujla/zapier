71b993150830c86c34a68b9ae8d535ec0e78fdf9
Sahib.Aujla <sahibpreet568@gmail.com>
2024-09-24 18:36:04 -0400

    added processor logic

--- apps/processor/src/index.ts
+++ apps/processor/src/index.ts
@@ -1,10 +1,42 @@
 import { Kafka } from "kafkajs";
+import { PrismaClient } from '@repo/db';
 
+const client = new PrismaClient();
+const TOPIC_NAME = 'zap-events';
 const kafka = new Kafka({
-  clientId: "outbox-processor",
-  brokers: ["localhost:9092"]
+    clientId: "outbox-processor",
+    brokers: ["localhost:9092"]
 })
 
-const producer = kafka.producer();
+
+async function main() {
+    const producer = kafka.producer();
+    producer.connect();
+    while (1) {
+        const processes = await client.zapRunOutbox.findMany({
+            take: 10
+        })
+
+        producer.send({
+            topic: TOPIC_NAME,
+            messages: processes.map(r => {
+                return {
+                    value: JSON.stringify({ zapRunId: r.zapRunId, stage: 0 })
+                }
+            })
+        })
+
+
+        await client.zapRunOutbox.deleteMany({
+            where: {
+                id: {
+                    in: processes.map(p => p.id),
+                }
+            }
+        })
+    }
+}
+
+main();
 
 
