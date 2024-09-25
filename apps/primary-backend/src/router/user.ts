d9812c8a337cb47e163006d708db877d7705c36e
Sahib.Aujla <sahibpreet568@gmail.com>
2024-09-25 14:50:53 -0400

    added jwt

--- apps/primary-backend/src/router/user.ts
+++ apps/primary-backend/src/router/user.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
-import { SignUpSchema } from '../zod/schema';
+import { SignInSchema, SignUpSchema } from '../zod/schema';
 import { PrismaClient } from '@repo/db';
+import jwt from 'jsonwebtoken';
+
 const router = Router();
 const client = new PrismaClient();
 
@@ -20,7 +22,18 @@ router.post('/signup', async (req, res) => {
                 email: parsedData.data.username
             }
         })
-        if (userExists) return res.status(403).json({ message: "user already exists. Please sign in" })
+        if (userExists) return res.status(403).json({ message: "user already exists. Please sign in" });
+
+        await client.user.create({
+            data: {
+                email: parsedData.data.username,
+                name: parsedData.data.name,
+                password: parsedData.data.password
+            }
+        })
+
+        return res.status(200).json({ message: "Used created successfully" });
+
 
     } catch (error) {
         return res.status(500).json({ message: "Server error" })
@@ -28,6 +41,24 @@ router.post('/signup', async (req, res) => {
 })
 
 router.post('/signin', async (req, res) => {
+    const body = req.body;
+    const parsedData = SignInSchema.safeParse(body);
+    if (!parsedData.success) return res.status(411).json({ message: "Invalid data" });
+
+    try {
+        const user = await client.user.findFirst({
+            where: {
+                email: parsedData.data.username,
+                password: parsedData.data.password
+            }
+        })
+        if (!user) return res.status(403).json({ message: "Incorrect username or password" });
+
+
+    } catch (error) {
+        return res.status(500).json({ message: "Server error" });
+    }
+
 
 })
 
