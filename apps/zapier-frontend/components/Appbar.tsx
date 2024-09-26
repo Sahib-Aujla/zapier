1d74530b4bdc2aaa2f49028d0297725a67afdd3d
Sahib.Aujla <sahibpreet568@gmail.com>
2024-09-26 18:53:32 -0400

    updated padding

--- apps/zapier-frontend/components/Appbar.tsx
+++ apps/zapier-frontend/components/Appbar.tsx
@@ -5,7 +5,7 @@ import PrimaryButton from "./buttons/PrimaryButton";
 
 const Appbar = () => {
   return (
-    <div className="flex justify-between border-b py-3">
+    <div className="flex justify-between border-b py-3 px-5">
       <div className="pl-4 text-2xl font-extrabold flex flex-col justify-center">
         Zapier
       </div>
