d333b3cf749c4a42fbd491ab43dc2008bdde2936
Sahib.Aujla <sahibpreet568@gmail.com>
2024-09-26 17:26:56 -0400

    added hero

--- apps/zapier-frontend/app/page.tsx
+++ apps/zapier-frontend/app/page.tsx
@@ -1,9 +1,11 @@
 import Appbar from "@/components/Appbar";
+import LandingPage from "@/components/landingPage/LandingPage";
 
 export default function Home() {
   return (
     <div className="">
       <Appbar />
+      <LandingPage />
     </div>
   );
 }
