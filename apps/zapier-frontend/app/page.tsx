1afa3c6d7465a1434651b107e6c4647a304eb8bc
Sahib.Aujla <sahibpreet568@gmail.com>
2024-09-26 18:59:34 -0400

    updated layout

--- apps/zapier-frontend/app/page.tsx
+++ apps/zapier-frontend/app/page.tsx
@@ -1,10 +1,8 @@
-import Appbar from "@/components/Appbar";
 import LandingPage from "@/components/landingPage/LandingPage";
 
 export default function Home() {
   return (
     <div className="">
-      <Appbar />
       <LandingPage />
     </div>
   );
