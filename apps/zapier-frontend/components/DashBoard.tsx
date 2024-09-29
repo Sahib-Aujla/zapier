b47d9457cdb643853914dea5e043434127bc9ebb
Sahib.Aujla <sahibpreet568@gmail.com>
2024-09-29 13:51:32 -0400

    added router

--- apps/zapier-frontend/components/DashBoard.tsx
+++ apps/zapier-frontend/components/DashBoard.tsx
@@ -2,17 +2,19 @@
 import React from "react";
 import DarkButton from "./buttons/DarkButton";
 import ZapTable from "./ZapTable";
+import { useRouter } from "next/navigation";
 
 //needs one button and one table
 // one button to show create and one to show the zaps
 const DashBoard = () => {
+  const router=useRouter();
   return (
     <div className="">
       <div className="w-full flex flex-col justify-center items-center pt-16">
         <div className="flex max-w-2xl w-full justify-between">
           <div className="text-3xl font-bold">My Zaps</div>
           <div>
-            <DarkButton type="big" onClick={() => {}}>
+            <DarkButton type="big" onClick={() => {router.push('/zap/create')}}>
               + Create
             </DarkButton>
           </div>
