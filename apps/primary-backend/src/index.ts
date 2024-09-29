5b74662260aad25a3e5a2a91971f99d60e66b1cb
Sahib.Aujla <sahibpreet568@gmail.com>
2024-09-29 13:56:29 -0400

    added endpoints for available actiosn and triggets

--- apps/primary-backend/src/index.ts
+++ apps/primary-backend/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from 'cors';
 import { userRouter } from "./router/user";
 import { zapRouter } from "./router/zap";
+import { triggerRouter } from "./router/trigger";
+import { actionRouter } from "./router/action";
 
 const app = express();
 
@@ -17,6 +19,10 @@ app.get('/', (req, res) => {
 
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/zap', zapRouter);
+app.use('/api/v1/trigger', triggerRouter);
+app.use('/api/v1/action', actionRouter);
+
+
 
 
 
--- apps/primary-backend/src/router/action.ts
+++ apps/primary-backend/src/router/action.ts
@@ -4,12 +4,12 @@ const client = new PrismaClient();
 const router = Router();
 router.get('/available', async (req, res) => {
     try {
-        const availableTriggers = await client.availableTrigger.findMany();
+        const availableActions = await client.availableAction.findMany();
 
-        return res.send(200).json(availableTriggers);
+        return res.send(200).json(availableActions);
     } catch (error) {
         return res.status(500).json({ "message": "Server Error" });
     }
 
 })
-export const triggerRouter = router;
+export const actionRouter = router;
